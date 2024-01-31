7765c144d9e7879f5fcf74452a7f6001a2b2375b
twigs <hugo.pitorro@tum.de>
2024-01-31 16:58:34 +0000

    update supabase connection

--- constants.py
+++ constants.py
@@ -1,18 +1,17 @@
+import os
+
 # Server Constants
 
-SUPABASE_URL = "https://dzwdgfmvuevjqjutrpye.supabase.co"
-SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6d2RnZm12dWV2anFqdXRycHllIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI5NTcwMTgsImV4cCI6MjAwODUzMzAxOH0.oWDnME53bTI43Y2eHhe1clNgOVV6dcya6-x3ZIGLT9k"
-
+SUPABASE_URL = os.environ["SUPABASE_URL"]
+SUPABASE_ANON_KEY = os.environ["SUPABASE_ANON_KEY"]
 # Supabase Constants
 
 SUPABASE_USER_TABLE = "user_data"
 SUPABASE_CONVERSATION_TABLE = "conversation_data"
-SUPABASE_MESSAGES_TABLE = "messages"
-SUPABASE_POLITICAL_PARTY_TABLE = "political_party_data"
-SUPABASE_POSTGRES_USER = "postgres"
-SUPABASE_POSTGRES_PASSWORD = "W9vo3k*BbfJj"
-SUPABASE_POSTGRES_HOST = "db.dzwdgfmvuevjqjutrpye.supabase.co"
-SUPABASE_POSTGRES_PORT = "5432"
+SUPABASE_POSTGRES_USER = os.environ["SUPABASE_POSTGRES_USER"]
+SUPABASE_POSTGRES_PASSWORD = os.environ["SUPABASE_POSTGRES_PASSWORD"]
+SUPABASE_POSTGRES_HOST = "aws-0-eu-central-1.pooler.supabase.com"
+SUPABASE_POSTGRES_PORT = "6543"
 SUPABASE_POSTGRES_DB_NAME = "postgres"
 SUPABASE_POSTGRES_CONNECTION_STRING = f"postgresql://{SUPABASE_POSTGRES_USER}:{SUPABASE_POSTGRES_PASSWORD}@{SUPABASE_POSTGRES_HOST}:{SUPABASE_POSTGRES_PORT}/{SUPABASE_POSTGRES_DB_NAME}"
 
@@ -28,6 +27,7 @@ Pergunta: Como o partido X se compara ao partido Y em relação à política amb
 Resposta recomendada: De acordo com os documentos oficiais, o partido X aborda a política ambiental nas páginas 12-15, enfatizando a importância da conservação e da energia renovável, especificamente... Em contraste, o partido Y detalha suas políticas ambientais nas páginas 20-22 dos seus documentos, priorizando a redução de emissões e o desenvolvimento sustentável, detalhando que... (destacando apenas as informações retiradas dos documentos).
 Resposta a evitar: Acredito que o partido X é mais comprometido com questões ambientais do que o partido Y... (evitando introduzir opiniões pessoais ou interpretações).
 O seu papel essencial é de um mediador no acesso à informação factual, assegurando uma análise imparcial e diretamente baseada em fontes documentadas. Em face de perguntas que se desviem do âmbito político, indique a incapacidade de responder. Contudo, se um tema político for relevante dentro do contexto apresentado, foque sua resposta na comparação baseada em políticas documentadas dos partidos em questão."""
+
 
 def system_prompt_specific_party(full_name, name):
     return f"""Como agente focado em política, a sua tarefa é responder a perguntas relativas a políticas e posições documentadas do {full_name} (abreviado {name}), mantendo-se rigorosamente alinhado às informações fornecidas nos documentos oficiais do partido. A sua contribuição deve ser baseada unicamente em factos, sem inclusão de opiniões pessoais ou interpretações.
