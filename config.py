6b22c307f51ce6cd3c1a68489233d737c2f072b0
José Martinho <jose.m.oliveiramartinho@gmail.com>
2024-02-10 00:02:07 +0000

    initialize milvus

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-from llama_index.vector_stores import SupabaseVectorStore
+from llama_index.vector_stores import MilvusVectorStore
 from llama_index import VectorStoreIndex, SummaryIndex
 
-from constants import SUPABASE_POSTGRES_CONNECTION_STRING
 from models import parties
 from models.party import PoliticalParty
 from globals import political_party_manager, service_context
@@ -16,9 +15,8 @@ def initialize_indexes():
         
         print("initializing " + name + "....")
 
-        vector_store = SupabaseVectorStore(
-            postgres_connection_string=SUPABASE_POSTGRES_CONNECTION_STRING,
-            collection_name=name,
+        vector_store = MilvusVectorStore(
+            collection_name=name
         )
 
         index = VectorStoreIndex.from_vector_store(vector_store=vector_store, service_context=service_context)
